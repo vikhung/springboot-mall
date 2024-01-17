9a6230f24776e40ac6bf2116e6cfa155e7201cf3
vikhung <vikhung@gmail.com>
2024-01-18 00:04:57 +0800

    springboot project first commit;

---
+++ src/main/java/tw/com/vik/SpringbootMallApplication.java
@@ -0,0 +1,13 @@
+package tw.com.vik;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootMallApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootMallApplication.class, args);
+	}
+
+}
