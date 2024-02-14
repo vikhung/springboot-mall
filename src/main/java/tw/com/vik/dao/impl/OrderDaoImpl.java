5ddbd21f5e84aee880b95429d6d057c2f9282b62
vikhung <vikhung@gmail.com>
2024-02-15 00:28:22 +0800

    add unit test

--- src/main/java/tw/com/vik/dao/impl/OrderDaoImpl.java
+++ src/main/java/tw/com/vik/dao/impl/OrderDaoImpl.java
@@ -107,7 +107,7 @@ public class OrderDaoImpl implements OrderDao
     public List<OrderItem> getOrderItemsByOrderId(Integer orderId)
     {
         String sql = "SELECT oi.order_item_id, oi.order_id, oi.product_id, oi.quantity, oi.amount, p.product_name, p.image_url "
-                + "FROM mall.order_item as oi "
+                + "FROM order_item as oi "
                 + "left join product as p "
                 + "on oi.product_id = p.product_id "
                 + "where oi.order_id = :orderId";
